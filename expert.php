b21d278ac244d827e0f32b0c91b9f368c960057c
Ellen Seghers <ellen.seghers1@gmail.com>
2022-02-15 14:59:48 +0100

    "exercise three finished"

--- expert.php
+++ expert.php
@@ -23,6 +23,7 @@ function new_exercise($x)
 
 new_exercise( 1);
 
+
 new_exercise(2);
 // === Exercise 2 ===
 // Below we create a week array with all days of the week.
@@ -32,3 +33,12 @@ $week = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "su
 $monday = $week[0];
 
 echo $monday;
+
+
+new_exercise(3);
+// === Exercise 3 ===
+// This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
+
+$str = "Debugged ! Also very fun";
+echo substr($str, 0, 10);
+
